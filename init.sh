e482db7972dfdaecefb59c390e18c8a133c4bb63
峯島和希 <finne@mineshimakazukinoMacBook-Pro.local>
2021-03-28 23:10:38 +0900

    fix

--- init.sh
+++ init.sh
@@ -2,12 +2,53 @@
 
 cd $(dirname $0)
 
+# check signed in to AppStore.
+echo -n "Do you signed in to AppStore? (y/N): "
+if read -q; then
+    echo "\nThank you. the application will be installed from AppStore with homebrew mas."
+else
+    echo "\nPlease sign in to install from AppStore with homebrew mas."
+    exit
+fi
+
 # install homebrew
 if which brew > /dev/null; then
     echo 'Homebrew already installed.'
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# application install with homebrew
+brew doctor
+brew bundle
+echo "please self install 'powermymac'"
+
+# execute defaults settings
+/bin/zsh ./defaults
+
+# enable aliases
+/bin/zsh ./aliases
+
+# skicka install and download .ssh from google drive
+anyenv install --init
+anyenv install goenv
+relogin
+
+GO_LATEST_VERSION=$(goenv install -l | grep -v - | tail -1)
+goenv install $GO_LATEST_VERSION
+goenv global $GO_LATEST_VERSION
+
+go get github.com/google/skicka
+skicka init
+skicka download .ssh ~/.ssh
+
+# vim settings
+mkdir ~/.vim
+mkdir ~/.vim/colors
+cp vim/vimrc ~/.vimrc
+cp vim/gvimrc ~/.gvimrc
+cp vim/colors/molokai.vim ~/.vim/colors/molokai.vim
 
 # install zprezto
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
@@ -40,33 +81,8 @@ echo "$(cat ./aliases)" >> ~/.zshrc
 # source .zshrc
 source ~/.zshrc
 
-# application install
-brew doctor
-brew bundle
-
-echo "please self install 'powermymac'"
-
-# execute defaults settings
-/bin/bash ./defaults
-
-# skicka install and download .ssh from google drive
-anyenv install goenv
-relogin
-
-GO_LATEST_VERSION=$(goenv install -l | grep -v - | tail -1)
-goenv install $GO_LATEST_VERSION
-goenv global $GO_LATEST_VERSION
-
-go get github.com/google/skicka
-skicka init
-skicka download .ssh ~/.ssh
-
-# vim settings
-mkdir ~/.vim
-mkdir ~/.vim/colors
-cp vim/vimrc ~/.vimrc
-cp vim/gvimrc ~/.gvimrc
-cp vim/colors/molokai.vim ~/.vim/colors/molokai.vim
+# open iterm2 to setting zprezto
+open /Applications/iTerm.app
 
 echo "finished !"
 
